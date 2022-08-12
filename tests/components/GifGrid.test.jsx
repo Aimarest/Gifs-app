b28a73b330b5adab146d24fb47ba8eb9fc3717f7
Ana <amarcosestevez@gmail.com>
2022-08-12 10:06:19 +0200

    add another test to GifGrid component

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,16 +1,50 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
 
+jest.mock('../../src/hooks/useFetchGifs')
 describe('Pruebas en el componente GifGrid', () => {
    const category = 'Friends'; 
 
     test('Debe de mostrar el loading inicialmente', () => {
 
-render( <GifGrid category = { category } /> );
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading: true,
+        })
 
-expect( screen.getByText( 'Cargando imágenes...' ) );
-expect( screen.getByText( category ) );
+        render(<GifGrid category={category} />);
+
+        expect(screen.getByText('Cargando imágenes...'));
+        expect(screen.getByText(category));
 
 
      })
+
+
+     test('Debe mostrar los gifs después de que se realice el fetch', () => { 
+
+
+        const gifs = [
+            {
+                id:'ABD',
+                title:'Doraemon',
+                url:'http://localhost/doraemon.jpg'
+            },
+
+            {
+                id:'kasdñj',
+                title:'Doraemon',
+                url:'http://localhost/doraemon.jpg'
+            }
+        ]
+        
+         useFetchGifs.mockReturnValue({
+             images: gifs,
+             isLoading: false,
+         })
+
+        render( <GifGrid category = { category } /> );
+      expect( screen.getAllByRole('img').length).toBe(2);
+      })
 });
